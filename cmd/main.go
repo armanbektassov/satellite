4c8e4aeb03c2ad51df87e583688286e6d19c9bb7
Arman Bektassov <armanbektassov@gmail.com>
2025-09-16 15:39:06 +0500

    update CI configuration to set up Go and golangci-lint, add health check endpoint, and change server port to 8080

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,9 +11,14 @@ func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt)
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "OK")
+	})
+
 	go func() {
-		http.ListenAndServe("0.0.0.0:6060", nil)
+		if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+			panic(err)
+		}
 	}()
 	<-done
-
 }
